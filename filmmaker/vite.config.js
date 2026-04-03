df87e31dfaaebf5c050f001dffe9eda355d21aea
ASWIN S <asenthilkumar@improva.io>
2026-04-03 15:11:13 +0530

    Set up GitHub Pages deployment

--- filmmaker/vite.config.js
+++ filmmaker/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'film-portfolio'
+
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  base: command === 'build' ? `/${repoName}/` : '/',
+}))
